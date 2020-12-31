3bb09348ab6275f61dd6ca4a05f39a2c8835c7da
Jung Ho Lee <alexxleee@gmail.com>
2020-12-31 23:10:21 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,5 +1,6 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
-  for 0...array.length
-    return 
+  for i in 0...array.length
+    return i if array[i] == value_to_find
+  end
 end
